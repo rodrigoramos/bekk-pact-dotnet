ebec6cf4ead6dcf4f15130d55c2ac42b843e51b7
Rodrigo Ramos <rodrigo.ramos@lambda3.com.br>
2020-09-10 01:29:20 -0300

    Allow multiples erros during body validation

--- src/nPact.Provider/Model/Validation/ResponseBodyJsonValidator.cs
+++ src/nPact.Provider/Model/Validation/ResponseBodyJsonValidator.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using System.Text;
 using nPact.Common.Contracts;
 using nPact.Common.Extensions;
 using Newtonsoft.Json.Linq;
@@ -7,24 +9,24 @@ namespace nPact.Provider.Model.Validation
 {
     public class ResponseBodyJsonValidator
     {
-        private readonly IProviderConfiguration configuration;
+        private readonly IProviderConfiguration _configuration;
+
         public ResponseBodyJsonValidator(IProviderConfiguration configuration)
-        {
-            this.configuration = configuration;
-        }
+            => _configuration = configuration;
+
         public string Validate(JContainer expected, string actual)
         {
-            if(string.IsNullOrWhiteSpace(actual))
-            {
+            if (string.IsNullOrWhiteSpace(actual))
                 return expected.IsNullOrEmpty() ? null : "Body is not supposed to be empty.";
-            }
-            switch (expected)
+
+            return expected switch
             {
-                case JObject o: return ValidateBodyAsObject(actual, o);
-                case JArray a: return ValidateBodyAsArray(actual, a);
-            }
-            return null;
+                JObject o => ValidateBodyAsObject(actual, o),
+                JArray a => ValidateBodyAsArray(actual, a),
+                _ => null
+            };
         }
+
         private string ValidateBodyAsObject(string actual, JObject expected)
         {
             JObject actualJson;
@@ -32,15 +34,26 @@ namespace nPact.Provider.Model.Validation
             {
                 actualJson = JObject.Parse(actual);
             }
-            catch(Newtonsoft.Json.JsonReaderException)
+            catch (Newtonsoft.Json.JsonReaderException)
             {
                 return "Body is not parsable to object";
             }
-            return expected.AsJEnumerable().Select(token => {
-                var actualToken = actualJson.GetValue(token.Path, configuration.BodyKeyStringComparison.GetValueOrDefault());
-                var expectedValue = expected.GetValue(token.Path);
-                return  ValidateTokens(actualToken, expectedValue);
-            }).FirstOrDefault(r => r != null);
+
+            var expectedTokens = expected.AsJEnumerable();
+
+            var validationMessages = expectedTokens
+                .Select(token =>
+                {
+                    var actualToken =
+                        actualJson.GetValue(token.Path, _configuration.BodyKeyStringComparison.GetValueOrDefault());
+                    var expectedValue = expected.GetValue(token.Path);
+                    return ValidateTokens(actualToken, expectedValue);
+                }).Where(r => r != null)
+                .ToArray();
+
+            return validationMessages.Any()
+                ? validationMessages.Aggregate((f, s) => $"{f}{Environment.NewLine}{s}")
+                : null;
         }
 
         private string ValidateBodyAsArray(string actual, JArray expected)
@@ -50,59 +63,68 @@ namespace nPact.Provider.Model.Validation
             {
                 actualJson = JArray.Parse(actual);
             }
-            catch(Newtonsoft.Json.JsonReaderException)
+            catch (Newtonsoft.Json.JsonReaderException)
             {
                 return "Body is not parsable to array";
             }
+
             return ValidateArrays(actualJson, expected);
         }
 
         private string ValidateArrays(JArray actual, JArray expected)
         {
             if (!expected.HasValues)
-            {
-                if (actual.HasValues)
-                {
-                    return $"Array is supposed to be empty at {expected.Path} in body.";
-                }
-                return null;
-            }
+                return actual.HasValues ? $"Array is supposed to be empty at {expected.Path} in body." : null;
+
             if (!actual.HasValues)
-            {
                 return $"Array is not supposed to be empty at {expected.Path} in body.";
-            }
-            var e = expected.GetEnumerator();
-            var a = actual.GetEnumerator();
+
+            var errors = new StringBuilder();
+            using var e = expected.GetEnumerator();
+            using var a = actual.GetEnumerator();
             while (e.MoveNext())
             {
                 if (!a.MoveNext()) return $"Element not found in array. Expected {e.Current} at {expected.Path}.";
                 var error = ValidateTokens(a.Current, e.Current);
-                if (error != null) return error;
+                if (error != null)
+                    errors.AppendLine(error);
             }
-            if (a.MoveNext()) return $"Unexpected element found in array {a.Current} at {expected.Path}.";
-            return null;
+
+            if (a.MoveNext())
+                errors.AppendLine($"Unexpected element found in array {a.Current} at {expected.Path}.");
+
+            return errors.Length > 0 ? errors.ToString() : null;
         }
+
         private string ValidateTokens(JToken actual, JToken expected)
         {
             if (actual.IsNull() && expected.IsNull()) return null;
             if (actual.IsNull() && !expected.IsNull()) return $"Cannot find {expected.Path} in body.";
-            if (!actual.IsNull() && expected.IsNull()) return $"Expected null, but found {actual} at {actual.Path} in body.";
-            if (actual.Type != expected.Type) return $"Expected {expected}, but found {actual} at {expected.Path} in body.";
-            switch(expected)
+            if (!actual.IsNull() && expected.IsNull())
+                return $"Expected null, but found {actual} at {actual.Path} in body.";
+
+            if (actual.Type != expected.Type)
+                return $"Expected {expected}, but found {actual} at {expected.Path} in body.";
+
+            switch (expected)
             {
                 case JObject o:
                 {
-                    return o.Properties().Select(p => ValidateTokens(actual[p.Name],p.Value)).FirstOrDefault(r => r!=null);
+                    var errors = o.Properties()
+                        .Select(p => ValidateTokens(actual[p.Name], p.Value))
+                        .Where(r => r != null)
+                        .ToArray();
+
+                    return errors.Any()
+                        ? errors.Aggregate((f, s) => $"{f}{Environment.NewLine}{s}")
+                        : null;
                 }
                 case JArray a:
-                {
-                    return ValidateArrays((JArray)actual, a);
-                }
+                    return ValidateArrays((JArray) actual, a);
                 default:
-                {
-                    if(actual.ToString() != expected.ToString()) return $"Not match at {expected.Path} in body. Expected: {expected} but received {actual}."; 
-                    return null;
-                }
+                    return actual.ToString() != expected.ToString()
+                        ? $"Not match at {expected.Path} in body. Expected: {expected} but received {actual}."
+                        : null;
             }
         }
     }
