32a8c9a0b53887ee8cf7f9f3587ff194cfad82a9
Rodrigo Ramos <rodrigo.ramos@lambda3.com.br>
2020-09-24 02:03:30 -0300

    Fix Nested Objects Comparison

---
+++ tests/nPact.Consumer.Tests/Matching/BodyComparerTests.cs
@@ -0,0 +1,110 @@
+using System;
+using FluentAssertions;
+using Moq;
+using nPact.Common.Contracts;
+using nPact.Consumer.Contracts;
+using nPact.Consumer.Matching;
+using nPact.Consumer.Rendering;
+using Xunit;
+
+namespace nPact.Consumer.Tests.Matching
+{
+    public class BodyComparerTests
+    {
+        private Mock<IConsumerConfiguration> _configurationMock;
+
+        public BodyComparerTests()
+        {
+            _configurationMock = new Mock<IConsumerConfiguration>();
+            _configurationMock.Setup(x => x.BodyKeyStringComparison)
+                .Returns(StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void GivingTwoEqualsANestedObjects_WhenMatching_ShouldReturnTrue()
+        {
+            var expectedRequest = GetRequestDefinition(new {object1 = new {prop1 = "value1"}});
+            var actualRequest = GetRequestDefinition(new {object1 = new {prop1 = "value1"}});
+
+            var bodyComparer = new BodyComparer(expectedRequest, _configurationMock.Object);
+
+            bodyComparer.Matches(actualRequest).Should().BeTrue();
+        }
+
+        [Fact]
+        public void GivingTwoEqualsANestedObjects_WhenDiffing_ShouldReturnNull()
+        {
+            var expectedRequest = GetRequestDefinition(new {object1 = new {prop1 = "value1"}});
+            var actualRequest = GetRequestDefinition(new {object1 = new {prop1 = "value1"}});
+
+            var bodyComparer = new BodyComparer(expectedRequest, _configurationMock.Object);
+
+            bodyComparer.DiffGram(actualRequest).Should().BeNull();
+        }
+        
+        [Fact]
+        public void GivinAMissingPropertyObject_WhenMatching_ShouldReturnFalse()
+        {
+            var expectedRequest = GetRequestDefinition(new {object1 = new {prop1 = "value1"}});
+            var actualRequest = GetRequestDefinition(new {object1 = new {prop2 = "value2"}});
+
+            var bodyComparer = new BodyComparer(expectedRequest, _configurationMock.Object);
+
+            bodyComparer.Matches(actualRequest).Should().BeFalse();
+        }
+
+        [Fact]
+        public void GivinAMissingPropertyObject_WhenDiffing_ShouldReturnDifferenceDescription()
+        {
+            var expectedRequest = GetRequestDefinition(new {object1 = new {prop1 = "value1"}});
+            var actualRequest = GetRequestDefinition(new {object1 = new {prop2 = "value2"}});
+
+            var bodyComparer = new BodyComparer(expectedRequest, _configurationMock.Object);
+
+            bodyComparer.DiffGram(actualRequest)
+                .Should().ContainKey("object1.prop1")
+                .WhichValue?.ToString()
+                .Should().BeEquivalentTo(@"{
+  ""expected"": ""Member object1.prop1"",
+  ""actual"": ""Member missing""
+}");
+        }
+
+        [Fact]
+        public void GivingObjectsWithDifferentValues_WhenMatching_ShouldReturnFalse()
+        {
+            var expectedRequest = GetRequestDefinition(new {object1 = new {prop1 = "value-expected"}});
+            var actualRequest = GetRequestDefinition(new {object1 = new {prop1 = "value-actual"}});
+
+            var bodyComparer = new BodyComparer(expectedRequest, _configurationMock.Object);
+
+            bodyComparer.Matches(actualRequest).Should().BeFalse();
+        }
+
+        [Fact]
+        public void GivingObjectsWithDifferentValues_WhenDiffing_ShouldReturnDifferenceDescription()
+        {
+            var expectedRequest = GetRequestDefinition(new {object1 = new {prop1 = "value-expected"}});
+            var actualRequest = GetRequestDefinition(new {object1 = new {prop1 = "value-actual"}});
+
+            var bodyComparer = new BodyComparer(expectedRequest, _configurationMock.Object);
+
+            bodyComparer.DiffGram(actualRequest)
+                .Should().ContainKey("object1.prop1")
+                .WhichValue?.ToString()
+                .Should().BeEquivalentTo(@"{
+  ""expected"": ""value-expected"",
+  ""actual"": ""value-actual""
+}");
+        }
+
+        private static IPactRequestDefinition GetRequestDefinition(object requestBody)
+        {
+            var requestMock = new Mock<IPactRequestDefinition>();
+            requestMock.SetupGet(x => x.RequestBody)
+                .Returns(new JsonBody(requestBody));
+
+            return requestMock.Object;
+        }
+    }
+}
